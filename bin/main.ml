7bd2ba4707ed14f5aee1a42c534c550dfd9e1cef
Yoshihiro Imai <y.imai@proof-ninja.co.jp>
2024-11-17 18:39:07 +0900

    tweetボタンの設置とqueryパラメータの導入

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,11 @@
 open Js_of_ocaml
 open Ompu_lib
 
-(*let document = Dom_html.window##.document
 let js = Js.string
-*)
+
+let get_params () : (string * string) list = Url.Current.arguments
+let query params = List.assoc_opt "q" params
+
 let get_textarea () =
   match Dom_html.getElementById_coerce "text" Dom_html.CoerceTo.textarea with
   | Some textarea -> textarea
@@ -17,12 +19,36 @@ let onkeyup _event =
   draw text;
   Js._false
 
+let tw_btn text =
+  let button_text = "この楽譜をツイートする" in
+  let hashtag = "ompuOrg" in
+  let body = Printf.sprintf "譜面をみる → " in
+  let link =
+    let query =
+      Url.encode_arguments [("q", text)]
+    in
+    let base = "www.ompu.org" in
+    "https://" ^ base ^ "?" ^ query
+  in
+  Tweet_button.post_button ~link ~hashtag body button_text
+
+let default_text = "(FAd6)| (=cAE6) | (FAdc edBc) | A4 E4|"
+
 let onload _event =
+  let text =
+    match query (get_params ()) with
+    | None -> default_text
+    | Some q -> q
+  in
+  (get_textarea())##.value := js text;
   (* 現在のテキストエリアの楽譜をレンダリングする *)
-  let text = (get_textarea())##.value in
   draw text;
+
   (* テキストエリアが書き換わったらリアルタイムでレンダリングし直す *)
   (get_textarea())##.onkeyup := Dom_html.handler onkeyup;
+
+  (* Tweetボタンの設置 *)
+  (Dom_html.getElementById_exn "tweet")##.innerHTML := js (tw_btn text);
   Js._false
 
 let () =
---
+++ compile.sh
@@ -0,0 +1,1 @@
+dune b @dev
